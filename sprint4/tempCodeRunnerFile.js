f9f5d400484875bdc57c160f31f8db1445d04b94
LewisCouget <lewis_2051@hotmail.com>
2024-09-01 22:38:27 -0300

    clean

--- sprint4/tempCodeRunnerFile.js
+++
@@ -1,7 +0,0 @@
-const fruits = ['orange', 'apple', 'banana', 'apples', 'apple', 'mango'];
-
-function isItAnApple(arr) {
-  return arr.map(item => item === 'apple');
-}
-
-console.log(isItAnApple(fruits));
